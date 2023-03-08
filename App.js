5214f363e208e9f24a88b64c44b8ca31423eb22e
Saily Jadhav <sailyjadhav1310@gmail.com>
2023-03-09 01:58:00 +0530

    Igniting the app and understanding Parcel.

--- App.js
+++ App.js
@@ -1,3 +1,44 @@
+/*
+* Following are things done by Parcel:
+* HMR - Hot Module Replacement
+* File Watcher Algorithm in C++
+* Bundling
+* Minify
+* Cleaning the code
+* Dev and Prd build
+* Super fast build algo
+* Image Optimization
+* Caching while development
+* Compression
+* Compatible with old version of browser - polyfills
+* enable https on dev build as well cmd: npx parcel index.html --https
+* port number
+* Consistent Hashing Algorithmn
+* Zero Config
+*
+*
+* ------
+* Transitive dependencies: Pkg manager which takes care of transitive dependencies of our code. 
+Means example parcel has multiple dependecies of its own, further those dependencies have their dependencies
+and so on is a transitive ependencies.
+* How to make app compatble with multiple browsers: open browserslist.dev in google and we have browserslist 
+pkg in node modules, we will understand how to use that pkg in the website
+ex: "browswerslist" : [
+    means this app will supprt all browsers last 2 versions
+    "last 2 versions" 
+  ]
+
+
+
+
+
+*/
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement(
   "h1",
   {
