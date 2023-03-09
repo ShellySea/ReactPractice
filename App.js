3f4502ee4d1254ab1bdac84836dd15e2681a2ff6
Saily Jadhav <sailyjadhav1310@gmail.com>
2023-03-10 01:41:56 +0530

    Passing key as props, React Element, React Component and COMPONENT COMPOSITION

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* Using React.createElement */
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title1",
-    key: 'heading1'
-  },
-  "Heading"
-);
 
-/* Using JSX */
-const heading1 = (
-  <h1 className="h3" key="head3">Heading 1</h1>
+/* React element */
+const Title = (
+  <h1 className="h3" key="head3">React Element</h1>
 )
+
+/* React component - Normal JS function*/
+const Comp = () => {
+  return (
+    <div>
+      <h3>Component Composition</h3>
+    </div>
+  )
+}
+
+/* React Component: std convention to write its name is by starting with capital letter */
+/* using react element inside react component using {} */
+/* COMPONENT COMPOSITION : Using react component inside react component using </> or {Nameofcomp()} */
+const HeaderComponent1 = () => {
+  return (
+    <div>
+      {Title}
+      <Comp/> 
+      {Comp()}
+      <h1>Heading 1</h1>
+      <h1>Heading 2</h1>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading1);
+root.render(<HeaderComponent1/>);
